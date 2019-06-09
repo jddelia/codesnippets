466c0820666d7527c9ee380bac32878818d2df51
jddelia <jddelia123@gmail.com>
2019-06-08 21:03:20 -0400

    Fix href

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="about">
       <p className="aboutText">
-        This app was created by <a href="https://www.jdeliaportfolio.netlify.com">JDelia</a>.
+        This app was created by <a href="https://jdeliaportfolio.netlify.com/">JDelia</a>.
         CodeSnippets is a collection of useful snippets
         of code, and information regarding programming.
       </p>
