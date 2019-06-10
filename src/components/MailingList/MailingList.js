223c9cbd2431dcd8524ddb4b06b5bc7eb36a376b
jddelia <jddelia123@gmail.com>
2019-06-09 20:28:30 -0400

    Make SnippetPage responsive

---
+++ src/components/MailingList/MailingList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MailingList = () => {
+  return (
+    <div className="mailingList">
+      <span>Want to Receive the Latest Snippets?</span>
+      <span>Join The Mailing List!</span>
+
+    </div>
+  );
+};
+
+export default MailingList;
